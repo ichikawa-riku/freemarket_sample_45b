8286a3c79ec711231b9987cbdd627ba43d9b9115
ichikawa-riku <owner@M15-108.local>
2019-03-02 13:03:30 +0900

    edit deproy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.11.0'
 
 set :application, 'freemarket_sample_45b'
-set :repo_url,  'git@github.com:Ykum4/freemarket_sample_45b.git'
+set :repo_url,  'git@github.com:ichikawa-riku/freemarket_sample_45b.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
