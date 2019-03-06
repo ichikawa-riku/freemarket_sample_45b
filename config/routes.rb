257e0bb242c9472068bf65c7a33c723500a26259
daiki0386 <d.hosokawa0806@gmail.com>
2019-03-06 12:38:05 +0900

    Create-signout-page
    #  what
    ログアウトページの作成
    
    指摘のあった、ヘッダーとフッターのhamlを部分テンプレ内に移す作業もやってます。
    あと、出品ボタンも部分テンプレ化してます。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'products#index'
+  get  'users/signout'  =>  'users#signout'
   resources :products, only: [:show, :new, :create] do
     resources :purchases, only: [:new, :create]
   end
