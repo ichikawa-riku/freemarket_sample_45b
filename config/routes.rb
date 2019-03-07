10c426b24594518d56690a9a6540d9a346319bc1
ichikawa-riku <owner@M15-108.local>
2019-03-08 03:53:12 +0900

    MarkUpListPage

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resources :purchases, only: [:new, :create]
   end
 
+  resources :categories, only: [:index, :show] do
+  end
+
   resources :users, only: [:new, :show, :edit] do
     collection do
       get 'signout'
