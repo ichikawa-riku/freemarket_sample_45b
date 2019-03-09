d5e14f162eaf9aa35907eda3bf1cf07fe776d5db
097yuji <g621308@gmail.com>
2019-03-09 20:35:17 +0900

    Faker導入
    ・fakerのgemを導入
    ・テスト用のダミーデータをfakerにて生成するようspec/factories/users.rbへ記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    nickname              { Faker::Internet.user_name(1..20)}
+    email                 { Faker::Internet.email }
+    password = Faker::Internet.password(8)
+    password              { password }
+    password_confirmation { password }
   end
 
 end
