67111fa139d4af7ba2fe48316c80db3cd53b0b91
097yuji <g621308@gmail.com>
2019-03-09 11:01:13 +0900

    プロフィール画面でニックネームと自己紹介文を編集更新できる機能
    user/id/edit のページでnicknameとintroductionを入力・更新できるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(user_params)
+    end
+    redirect_to :action => "edit"
   end
 
   def signout
   end
 
   def identification
+  end
+
+  private
+  def user_params
+    params.permit(:nickname, :introduction)
   end
 
 end
