5fbebdc0d6dcc27d86cd1c04a6f2e3e8bcdb9fd4
097yuji <g621308@gmail.com>
2019-03-09 14:22:37 +0900

    プロフィール画面で編集できるやつ
    プロフィール画面のニックネームと自己紹介文を編集できるように実装したやつ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:nickname, :introduction)
+    params.require(:user).permit(:nickname, :introduction)
   end
 
 end
