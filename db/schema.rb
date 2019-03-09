1b021c76c5d84da94f84cd1857acbc26204940e0
ktr-tkhs <ktr.0456.tkhs@gmail.com>
2019-03-09 16:33:16 +0900

    add drop_table_conditions-migration-file
    ・マイグレーションファイルdrop_table_conditionsの作成
    ・モデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190308121644) do
+ActiveRecord::Schema.define(version: 20190309072351) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "prefecture", null: false
@@ -18,10 +18,6 @@ ActiveRecord::Schema.define(version: 20190308121644) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
-  end
-
-  create_table "conditions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "status", null: false
   end
 
   create_table "product_sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
