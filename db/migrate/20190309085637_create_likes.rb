3c8ee375bf3d57b544752e6ad6879bee06d4c482
ktr-tkhs <ktr.0456.tkhs@gmail.com>
2019-03-10 11:52:38 +0900

    Revert "Merge remote-tracking branch 'Ykum4/master'"
    This reverts commit 44a051e62f598c00549ad21c5ad871fd9b6eb547, reversing
    changes made to 99a36f6699fdbbf62bae50c1a788ac99e8877e1b.

--- db/migrate/20190309085637_create_likes.rb
+++
@@ -1,8 +0,0 @@
-class CreateLikes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :likes do |t|
-      t.references  :user,    null:false, foreign_key: true
-      t.references  :product, null:false, foreign_key: true
-    end
-  end
-end
