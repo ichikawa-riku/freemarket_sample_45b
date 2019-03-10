890b2685108d69b1afd200f733c4263e9c0ecb08
ktr-tkhs <ktr.0456.tkhs@gmail.com>
2019-03-10 20:32:40 +0900

    clean migration file

--- db/migrate/20190309085637_create_likes.rb
+++ db/migrate/20190309085637_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
-      t.references  :user,    null:false, foreign_key: true
-      t.references  :product, null:false, foreign_key: true
+      t.references  :user,    null: false, foreign_key: true
+      t.references  :product, null: false, foreign_key: true
     end
   end
 end
