dd8353e4ddc256a5d467c3017a5ca58f7462e68c
ichikawa-riku <owner@M15-108.local>
2019-03-13 02:20:17 +0900

    AddAddImages
    ・商品画像を登録できるようにした

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,12 +11,12 @@ class ProductsController < ApplicationController
 #商品出品ページ
   def new
     @product = Product.new
+    @product.product_images.build
   end
 
 #トップページへリダイレクト
   def create
     @product = Product.new(product_params)
-    binding.pry
     if @product.save
       redirect_to root_path
     else
@@ -26,6 +26,6 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :product_size_id, :condition, :shipping_method, :shipping_burden, :area_id, :estimated_date, :price).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :description, :category_id, :product_size_id, :brand_id, :condition, :shipping_method, :shipping_burden, :area_id, :estimated_date, :price, product_images_attributes:[:image]).merge(user_id: 1)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   belongs_to :product_size
   belongs_to :brand
   has_many :product_images
+  accepts_nested_attributes_for :product_images, limit: 10
   belongs_to :category
 
   enum condition:       {"新品、未使用": 0, "未使用に近い": 1, "目立った傷や汚れなし": 2, "やや傷や汚れあり": 3, "傷や汚れあり": 4, "全体的に状態が悪い": 5}
