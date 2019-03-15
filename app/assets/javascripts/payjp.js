e9986134c425b3d9f80e143a8664c85bb331c54f
Ykum4 <ykuma@YusukenoMacBook-Pro.local>
2019-03-15 20:59:26 +0900

    Delete console.log

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -10,7 +10,6 @@ $(document).on('turbolinks:load', function(){
       exp_month : parseInt($('#payment_card_expire_mm').val()),
       exp_year : parseInt($('#payment_card_expire_yy').val())
     };
-    console.log(card);
     Payjp.createToken(card, function(status, response){
       if (status === 200) {
         $('.form-group-card-num__input').removeAttr("name");
@@ -18,7 +17,6 @@ $(document).on('turbolinks:load', function(){
         $('.form-group-card-expire__input').removeAttr("name");
         $('.form-group-card-num__input').removeAttr("name");
         var token = response.id;
-        console.log(token);
         form.append($('<input type="hidden" name="payjp_token" class="payjp-token" />').val(token));
         form.get(0).submit();
       }else {
