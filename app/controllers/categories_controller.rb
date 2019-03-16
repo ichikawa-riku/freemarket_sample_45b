700e0b0626433b4980b392339e5582f320f98f2a
ichikawa-riku <owner@M15-108.local>
2019-03-16 21:00:43 +0900

    AddCategoryPage
    カテゴリーページ表示機能の実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,17 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    if @category.main_category_id.nil? && @category.sub_category_id.nil?
+      @relation_categories = Category.where("main_category_id = #{@category.id} and sub_category_id IS NOT NULL").limit(14)
+      @products = Product.joins(:category).merge(Category.where(main_category_id: @category.id)).order("created_at DESC")
+    elsif @category.main_category_id.present? && @category.sub_category_id.nil?
+      @relation_categories = Category.where(["main_category_id = ? and sub_category_id = ?", @category.main_category_id, @category.id]).limit(14)
+      @products = Product.joins(:category).merge(Category.where(sub_category_id: @category.id)).order("created_at DESC")
+    else
+      @relation_categories = Category.where(["main_category_id = ? and sub_category_id = ?", @category.main_category_id, @category.sub_category_id]).limit(14)
+      @products = @category.products.order("created_at DESC")
+    end
   end
 
   def sub_category
