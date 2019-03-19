d861fef2a5fc07e3eea11f89f55c43820c5b4102
ichikawa-riku <owner@M15-108.local>
2019-03-19 21:26:45 +0900

    Edit Controller
    ・ルーティングの設定
    ・コントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update]
+  before_action :set_cuttenr_user, only: [:published, :trading, :sold]
   before_action :authenticate_user!, except: :new
   def new
   end
@@ -26,6 +27,18 @@ class UsersController < ApplicationController
   def identification
   end
 
+  def published
+    @products = @user.products.where(status: 0..1)
+  end
+
+  def trading
+    @products = @user.products.where(status: 2)
+  end
+
+  def sold
+    @products = @user.products.where(status: 3)
+  end
+
   private
   def user_params
     params.require(:user).permit(:nickname, :introduction)
@@ -33,6 +46,10 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def set_cuttenr_user
+    @user = User.find(current_user.id)
   end
 
 end
