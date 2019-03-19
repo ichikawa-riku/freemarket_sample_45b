b8a7d39dcd9df18254525240cea3c7198f58703d
ichikawa-riku <owner@M15-108.local>
2019-03-19 14:54:37 +0900

    Edit categories_controller.rb
    ・@category = Category.find(params[:id])をset型にした
    ・whereをプレースホルダー型ではなく通常の条件に

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
   def index
   end
 
   def show
-    @category = Category.find(params[:id])
     if @category.main_category_id.nil? && @category.sub_category_id.nil?
       @relation_categories = Category.where("main_category_id = #{@category.id} and sub_category_id IS NOT NULL").limit(14)
       @products = Product.joins(:category).merge(Category.where(main_category_id: @category.id)).order("created_at DESC")
@@ -11,7 +12,7 @@ class CategoriesController < ApplicationController
       @relation_categories = Category.where(["main_category_id = ? and sub_category_id = ?", @category.main_category_id, @category.id]).limit(14)
       @products = Product.joins(:category).merge(Category.where(sub_category_id: @category.id)).order("created_at DESC")
     else
-      @relation_categories = Category.where(["main_category_id = ? and sub_category_id = ?", @category.main_category_id, @category.sub_category_id]).limit(14)
+      @relation_categories = Category.where(sub_category_id: @category.sub_category_id).limit(14)
       @products = @category.products.order("created_at DESC")
     end
   end
@@ -29,4 +30,11 @@ class CategoriesController < ApplicationController
       format.json
     end
   end
+
+private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+
 end
